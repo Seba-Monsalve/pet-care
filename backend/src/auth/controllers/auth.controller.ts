d3f24e757456a67f7211985a48927ccb7bcb0d8f
Seba Monsalve <sebastian.monsalve@usach.cl>
2025-05-14 21:03:47 -0400

    --amend

--- backend/src/auth/controllers/auth.controller.ts
+++ backend/src/auth/controllers/auth.controller.ts
@@ -111,7 +111,7 @@ export class AuthController {
         .cookie("access_token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          maxAge: 7 * 24 * 60 * 60 * 1000,
+          maxAge: 60 * 60 * 1000, //
           sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         })
         .status(200)
